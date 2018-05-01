ab5b3156fdf7c18af59753aa77a5915ec25ecc53
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-05-01 13:38:04 -0700

    script the running of esg-autoinstall

--- scripts/run_2.x_esg_autoinstall.py
+++ scripts/run_2.x_esg_autoinstall.py
@@ -44,7 +44,7 @@ if status != SUCCESS:
 
 #cmd = "sudo script -c '/usr/local/bin/esg-autoinstall' {w}/installation.log".format(w=workdir)
 #cmd = "sudo -- sh -c 'export TERM=vt100; /usr/local/bin/esg-autoinstall'"
-cmd = "sudo bash -c 'export TERM=vt100; /usr/local/bin/esg-autoinstall'"
+cmd = "sudo bash -c \"export TERM=vt100; script -c '/usr/local/bin/esg-autoinstall' installation.log\"""
 status = run_cmd(cmd, True, False, True)
 if status != SUCCESS:
     sys.exit(status)
