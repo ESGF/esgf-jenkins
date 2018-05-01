25c2692232ad2c0faa7c1f1090e1194698b18b13
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-05-01 14:13:08 -0700

    try sudo -- sh -c

--- scripts/run_2.x_esg_autoinstall.py
+++ scripts/run_2.x_esg_autoinstall.py
@@ -45,7 +45,9 @@ if status != SUCCESS:
 #cmd = "sudo script -c '/usr/local/bin/esg-autoinstall' {w}/installation.log".format(w=workdir)
 #cmd = "sudo -- sh -c 'export TERM=vt100; /usr/local/bin/esg-autoinstall'"
 #cmd = "sudo bash -c 'export TERM=vt100; script -c /usr/local/bin/esg-autoinstall installation.log"
-cmd = "sudo -- sh -c '/usr/local/bin/esg-autoinstall'"
+###cmd = "sudo bash -c 'export TERM=vt100; /usr/local/bin/esg-autoinstall installation.log"
+
+cmd = "sudo -- sh -c 'export TERM=vt100; /usr/local/bin/esg-autoinstall'"
 
 status = run_cmd(cmd, True, False, True)
 if status != SUCCESS:
