e4151fe1028b73e10a66a6fa050a23a3588829aa
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-05-02 14:03:54 -0700

    added sys.stdout.flush() in run_2.x_esg_autoinstall.py

--- scripts/run_2.x_esg_autoinstall.py
+++ scripts/run_2.x_esg_autoinstall.py
@@ -65,15 +65,15 @@ status = run_cmd(cmd, True, True, True)
 if status != SUCCESS:
     sys.exit(status)
 
-sys.stdout.flush()
+#sys.stdout.flush()
 
 cmd = "grep 'Node installation is complete.' {w}/installation.log".format(w=workdir)
-status = run_cmd(cmd, True, False, True)
+status = run_cmd(cmd, True, True, True)
 
 # touch a file
-cmd = "touch {w}/esg-autoinstall.COMPLETE".format(w=workdir)
-status1 = run_cmd(cmd, True, True, True)
-sys.stdout.flush()
+#cmd = "touch {w}/esg-autoinstall.COMPLETE".format(w=workdir)
+#status1 = run_cmd(cmd, True, True, True)
+#sys.stdout.flush()
 print("xxx...exiting from run_2.x_esg_autoinstall.py...")
 sys.exit(status)
 
