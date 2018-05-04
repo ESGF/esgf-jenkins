f0d550bce4e3f72818eddb2ca22bd71b60cc5710
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-05-03 22:32:01 -0700

    added scripts/prep_2.x_post_install.py

--- scripts/run_2.x_post_install_steps.py
+++ scripts/run_2.x_post_install_steps.py
@@ -11,12 +11,10 @@ from Util import *
 parser = argparse.ArgumentParser(description="run esgf 2.x post install steps",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument("-n", "--vm_node", required=True, help="node to copy keypair from")
 parser.add_argument("-d", "--dir", required=True, help="directory on master where keypair and other files for vm node will be copied from")
 parser.add_argument("-H", "--vm_jenkins_home", required=True, help="vm user jenkins home directory")
 
 args = parser.parse_args()
-vm_node = args.vm_node
 dir = args.dir
 workdir = args.vm_jenkins_home
 
@@ -87,8 +85,8 @@ if status != SUCCESS:
 #
 # reboot vm
 #
-print("REBOOTING...{n}".format(n=vm_node))
-cmd = "sudo reboot".format(n=vm_node)
+print("REBOOTING...")
+cmd = "sudo reboot"
 status = run_cmd(cmd, True, False, True)
 if status != SUCCESS:
     sys.exit(status)
@@ -99,7 +97,7 @@ time.sleep(15)
 #cmd = "ssh -t -t {n} sudo bash -c 'export TERM=vt100; /usr/local/bin/esg-node start'".format(n=vm_node)
 
 #cmd = "ssh -t -t jenkins@{n} sudo bash -c 'export TERM=vt100; /usr/local/bin/esg-node start'".format(n=vm_node)
-cmd = "sudo /usr/local/bin/esg-node start".format(n=vm_node)
+cmd = "sudo /usr/local/bin/esg-node start"
 status = run_cmd(cmd, True, False, True)
 sys.exit(status)
 
