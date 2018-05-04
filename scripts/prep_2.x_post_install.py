1202ecc68c1dcc88cb35336cb51281c7c706847c
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-05-03 22:19:14 -0700

    added scripts/prep_2.x_post_install.py

---
+++ scripts/prep_2.x_post_install.py
@@ -0,0 +1,44 @@
+import sys
+import os
+import argparse
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+modules_dir = os.path.join(this_dir, '..', 'modules')
+sys.path.append(modules_dir)
+
+from Util import *
+
+parser = argparse.ArgumentParser(description="prep for 2.x post install",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("-n", "--vm_node", required=True, help="node to copy keypair from")
+parser.add_argument("-d", "--dir", required=True, help="directory on master where keypair and other files for vm node will be copied from")
+
+args = parser.parse_args()
+vm_node = args.vm_node
+dir = args.dir
+
+sys.stdout.flush()
+
+#
+# run this on master node as user 'jenkins'
+#
+
+cmd = "scp {dir}/keypair.tar {n}:/tmp".format(dir=dir, n=vm_node)
+status = run_cmd(cmd, True, False, True)
+if status != SUCCESS:
+    sys.exit(status)
+
+cmd = "scp {d}/tomcat.server.xml {n}:/tmp/server.xml".format(n=vm_node, d=dir)
+#cmd = "scp {d}/tomcat.server.xml {n}:{dest_file}".format(d=dir, n=vm_node,
+#                                                         dest_file=dest_file)
+status = run_cmd(cmd, True, False, True)
+if status != SUCCESS:
+    sys.exit(status)
+
+dest_file = "/etc/httpd/conf/esgf-httpd.conf"
+cmd = "scp {d}/esgf-httpd.conf {n}:/tmp".format(n=vm_node, d=dir)
+status = run_cmd(cmd, True, False, True)
+
+sys.exit(status)
+
--- scripts/run_2.x_post_install_steps.py
+++ scripts/run_2.x_post_install_steps.py
@@ -8,7 +8,7 @@ sys.path.append(modules_dir)
 
 from Util import *
 
-parser = argparse.ArgumentParser(description="install esgf 2.x",
+parser = argparse.ArgumentParser(description="run esgf 2.x post install steps",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument("-n", "--vm_node", required=True, help="node to copy keypair from")
@@ -23,37 +23,30 @@ workdir = args.vm_jenkins_home
 sys.stdout.flush()
 
 #
-# run this on master node as user 'jenkins'
+# run this on vm node as user 'jenkins'
 #
-
 
 #
 # install keypair
 #
 
-cmd = "scp {dir}/keypair.tar {n}:/tmp".format(dir=dir, n=vm_node)
-status = run_cmd(cmd, True, False, True)
-if status != SUCCESS:
-    sys.exit(status)
-
-cmd = "ssh  {n} sudo bash  'cd /tmp; tar -xvf /tmp/keypair.tar'".format(n=vm_node)
+cmd = "sudo bash -c 'cd /tmp; tar -xvf keypair.tar'"
 status = run_cmd(cmd, True, False, True)
 if status != SUCCESS:
     sys.exit(status)
 
 # in case this is a rerun, and esgf-test-suite repo already exists
-cmd = "ssh  {n} rm -rf {workdir}/esgf-test-suite".format(n=vm_node, workdir=workdir)
+cmd = "rm -rf {workdir}/esgf-test-suite".format(workdir=workdir)
 status = run_cmd(cmd, True, False, True)
 if status != SUCCESS:
     sys.exit(status)
 
-cmd = "ssh  {n} git clone https://github.com/ESGF/esgf-test-suite".format(n=vm_node)
+cmd = "git clone https://github.com/ESGF/esgf-test-suite"
 status = run_cmd(cmd, True, False, True)
 if status != SUCCESS:
     sys.exit(status)
 
-cmd = "ssh  {n} sudo bash -c \"expect {w}/esgf-test-suite/scripts-llnl/auto-keypair.exp\"".format(w=workdir,
-                                                                                                    n=vm_node)
+cmd = "sudo bash -c \"expect {w}/esgf-test-suite/scripts-llnl/auto-keypair.exp\"".format(w=workdir)
 status = run_cmd(cmd, True, False, True)
 if status != SUCCESS:
     sys.exit(status)
@@ -62,33 +55,22 @@ if status != SUCCESS:
 # update /usr/local/tomcat/conf/server.xml
 #
 dest_file = '/usr/local/tomcat/conf/server.xml'
-cmd = "ssh {n} sudo mv {orig} {orig}.ORIG".format(n=vm_node,
-                                                  orig=dest_file)
+cmd = "sudo mv {orig} {orig}.ORIG".format(orig=dest_file)
 status = run_cmd(cmd, True, False, True)
 if status != SUCCESS:
     sys.exit(status)
 
-cmd = "scp {d}/tomcat.server.xml {n}:/tmp/server.xml".format(n=vm_node, d=dir)
-#cmd = "scp {d}/tomcat.server.xml {n}:{dest_file}".format(d=dir, n=vm_node,
-#                                                         dest_file=dest_file)
+cmd = "sudo cp /tmp/server.xml {dest_file}".format(dest_file=dest_file)
 status = run_cmd(cmd, True, False, True)
 if status != SUCCESS:
     sys.exit(status)
 
-cmd = "ssh {n} sudo cp /tmp/server.xml {dest_file}".format(n=vm_node,
-                                                           dest_file=dest_file)
+cmd = "sudo chown tomcat {dest_file}".format(dest_file=dest_file)
 status = run_cmd(cmd, True, False, True)
 if status != SUCCESS:
     sys.exit(status)
 
-cmd = "ssh {n} sudo chown tomcat {dest_file}".format(n=vm_node,
-                                                     dest_file=dest_file)
-status = run_cmd(cmd, True, False, True)
-if status != SUCCESS:
-    sys.exit(status)
-
-cmd = "ssh {n} sudo chgrp tomcat {dest_file}".format(n=vm_node,
-                                                     dest_file=dest_file)
+cmd = "sudo chgrp tomcat {dest_file}".format(dest_file=dest_file)
 status = run_cmd(cmd, True, False, True)
 if status != SUCCESS:
     sys.exit(status)
@@ -97,13 +79,7 @@ if status != SUCCESS:
 # updated /etc/httpd/conf/esgf-httpd.conf
 #
 dest_file = "/etc/httpd/conf/esgf-httpd.conf"
-cmd = "scp {d}/esgf-httpd.conf {n}:/tmp".format(n=vm_node, d=dir)
-status = run_cmd(cmd, True, False, True)
-if status != SUCCESS:
-    sys.exit(status)
-
-cmd = "ssh {n} sudo cp /tmp/esgf-httpd.conf {dest_file}".format(n=vm_node,
-                                                                dest_file=dest_file)
+cmd = "sudo cp /tmp/esgf-httpd.conf {dest_file}".format(dest_file=dest_file)
 status = run_cmd(cmd, True, False, True)
 if status != SUCCESS:
     sys.exit(status)
