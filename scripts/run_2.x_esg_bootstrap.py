11b3d15329571f714a2fe23bd1b01702e38a54b6
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-05-09 13:57:01 -0700

    adding jenkinsFile/esgf_2.x_install.devel.aims

--- scripts/run_2.x_esg_bootstrap.py
+++ scripts/run_2.x_esg_bootstrap.py
@@ -14,17 +14,27 @@ parser = argparse.ArgumentParser(description="install esgf 2.x",
 parser.add_argument("-d", "--dist", required=True, choices=['master', 'devel'],
                     help="distribution, 'devel' or 'master'")
 parser.add_argument("-v", "--version", required=True,
-                    help="distribution, 'devel' or 'master'")
+                    help="version, for ex: '2.6/7'")
+parser.add_argument("-m", "--mirror", required=True,
+                    help="mirror url, 'fr' or 'aims'")
 
 args = parser.parse_args()
 dist = args.dist
 version = args.version
+mirror = args.mirror
 
 # get esg-bootstrap
-base_url = "http://distrib-coffee.ipsl.jussieu.fr/pub/esgf/dist"
+
+if mirror == 'fr':
+    base_url = "http://distrib-coffee.ipsl.jussieu.fr/pub/esgf/dist"
+elif mirror == 'aims':
+    base_url = "https://aims1.llnl.gov/esgf/dist"
+
+
 bin_dir = "/usr/local/bin"
 if dist == 'devel':
-    url = "{b}/devel/{v}/esgf-installer/esg-bootstrap".format(b=base_url,
+    # TEMPORARY TEMPORARY remove the .aims
+    url = "{b}/devel/{v}/esgf-installer/esg-bootstrap.aims".format(b=base_url,
                                                              v=version)
 else:
     url = "{b}/{v}/esgf-installer/esg-bootstrap".format(b=base_url,
@@ -41,7 +51,10 @@ status = run_cmd(cmd, True, False, True, bin_dir)
 if status != SUCCESS:
     sys.exit(status)
 
-cmd = "sudo ./esg-bootstrap"
+if dist == 'devel':
+    cmd = "sudo ./esg-bootstrap --devel"
+else:
+    cmd = "sudo ./esg-bootstrap"
 status = run_cmd(cmd, True, False, True, bin_dir)
 
 sys.exit(status)
