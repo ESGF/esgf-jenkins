7855fcf7709156c97c657daff384ae673c634dbc
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-05-17 12:46:52 -0700

    adding run_esgf_test_suite.py

--- scripts/run_esgf_test_suite.py
+++ scripts/run_esgf_test_suite.py
@@ -14,23 +14,15 @@ parser = argparse.ArgumentParser(description="run esgf-test-suite",
 
 parser.add_argument("-b", "--branch", required=True, default='master',
                     help="git branch")
-parser.add_argument("-o", "--run_test_suite_options", required=True, default='master',
+parser.add_argument("-o", "--run_test_suite_options", required=True, 
+                    default='!compute,!cog_create_user,!slcs',
                     help="options to run testsuite with - this will be passed as value to -a option ")
 
 args = parser.parse_args()
 branch = args.branch
+run_options = args.run_test_suite_options
 
-def get_esgf_test_suite(branch='master'):
-    current_time = time.localtime(time.time())
-    time_str = time.strftime("%b.%d.%Y.%H:%M:%S", current_time)
-    user_home = os.environ['HOME']
-    workdir = "{home}/work/esgf-test-suite-{time_stamp}".format(home=user_home,
-                                                                time_stamp=time_str)
-    cmd = "mkdir -p {workdir}".format(workdir=workdir)
-    ret_code = run_cmd(cmd, True, False, True)
-    if ret_code != SUCCESS:
-        print("FAIL...{cmd}".format(cmd=cmd))
-        return ret_code
+def get_esgf_test_suite(workdir, branch='master'):
 
     if branch == 'master':
         cmd = "git clone https://github.com/ESGF/esgf-test-suite.git {d}".format(d=workdir)
@@ -38,7 +30,44 @@ def get_esgf_test_suite(branch='master'):
         cmd = "git clone -b {b} https://github.com/ESGF/esgf-test-suite.git {d}".format(d=workdir,
                                                                                         b=branch)
     
-    ret_code = run_cmd(cmd, True, False, True, workdir)
+    ret_code = run_cmd(cmd, True, False, True)
     return(ret_code)
 
-ret_code = get_esgf_test_suite(branch)
+
+def run_esgf_test_suite(workdir, run_options):
+
+    std_options = "--nocapture --nologcapture --with-html --with-id -v"
+    user_home = os.environ['HOME']
+    conf_file_options = "--tc-file {home}/configs/my_config.ini".format(home=user_home)
+    
+    cmd = "python esgf-test.py {std_opt} {conf_opt} -a {opts}".format(std_opt=std_options,
+                                                                      conf_opt=conf_file_options,
+                                                                      opts=run_options)
+    test_dir = "{w}/esgf-test-suite/esgf-test-suite".format(w=workdir)
+
+    ret_code = run_cmd(cmd, True, False, True, test_dir)
+    return(ret_code)
+
+
+current_time = time.localtime(time.time())
+time_str = time.strftime("%b.%d.%Y.%H:%M:%S", current_time)
+user_home = os.environ['HOME']
+workdir = "{home}/work/esgf-test-suite-{time_stamp}".format(home=user_home,
+                                                                time_stamp=time_str)
+cmd = "mkdir -p {workdir}".format(workdir=workdir)
+status = run_cmd(cmd, True, False, True)
+if status != SUCCESS:
+    print("FAIL...{cmd}".format(cmd=cmd))
+    sys.exit(status)
+
+status = get_esgf_test_suite(workdir, branch)
+if status != SUCCESS:
+    print("FAIL...get_esgf_test_suite")
+    sys.exit(status)
+
+status = run_esgf_test_suite(workdir, run_options)
+if status != SUCCESS:
+    print("FAIL...run_esgf_test_suite")
+
+sys.exit(status)
+
