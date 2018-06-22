8cc86ecfc2bf0a2c52860d8f3fa2003bb40e8488
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-06-22 13:32:05 -0700

    adding esgf_3.x_install

--- scripts/run_3.x_esg_bootstrap.py
+++ scripts/run_3.x_esg_bootstrap.py
@@ -35,7 +35,7 @@ if status != SUCCESS:
     sys.exit(status)
 
 repo_dir = "{workdir}/esgf-installer".format(workdir=workdir)
-cmd = "{repo_dir}/esg_bootstrap.sh".format(repo_dir=repo_dir)
+cmd = "sudo {repo_dir}/esg_bootstrap.sh".format(repo_dir=repo_dir)
 status = run_cmd(cmd, True, False, True, repo_dir)
 
 sys.exit(status)
