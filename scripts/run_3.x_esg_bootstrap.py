262dc98b1402bdd085abd9f142572705ae3e8183
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-11-28 16:53:42 -0800

    add -m to scripts/run_3.x_esg_bootstrap.py for migration

--- scripts/run_3.x_esg_bootstrap.py
+++ scripts/run_3.x_esg_bootstrap.py
@@ -17,10 +17,14 @@ parser.add_argument("-w", "--workdir", required=True,
 parser.add_argument("-b", "--branch", default='master',
                     help="branch of esgf-installer repo to use")
 
+parser.add_argument("-m", "--migrate", default=False, action="store_true",
+                    help="specify if doing migration")
+
 
 args = parser.parse_args()
 workdir = args.workdir
 branch  = args.branch
+migrate = args.migrate
 
 repo_url = "https://github.com/ESGF/esgf-installer.git"
 if branch == 'master':
@@ -35,7 +39,11 @@ if status != SUCCESS:
     sys.exit(status)
 
 repo_dir = "{workdir}/esgf-installer".format(workdir=workdir)
-cmd = "sudo {repo_dir}/esg_bootstrap.sh".format(repo_dir=repo_dir)
+if migrate:
+    cmd = "sudo {repo_dir}/esg_bootstrap.sh migrate".format(repo_dir=repo_dir)
+else:
+    cmd = "sudo {repo_dir}/esg_bootstrap.sh".format(repo_dir=repo_dir)
+
 status = run_cmd(cmd, True, False, True, repo_dir)
 sys.exit(status)
 
