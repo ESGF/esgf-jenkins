fd46c40c6af6cceb75502186a12a1a1401c33461
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-12-27 13:56:31 -0800

    add debug messages

--- scripts/run_publisher_test.py
+++ scripts/run_publisher_test.py
@@ -97,7 +97,7 @@ def run_import_test(esgf_conda_env):
 
     cmd = "{e}; python -c 'import esgcet.config.cmip6_handler'".format(e=set_env)
     ret_code = run_in_conda_env_as_root(conda_path, esgf_conda_env, cmd)
-    return(ret_cod)
+    return(ret_code)
 
 #
 # main code
