976318b4a54fbdb9f450ced4f379b57648b61971
Muryanto <muryanto1@ML-9516678.llnl.gov>
2019-01-28 11:53:00 -0800

    update to remove pcmdi8

--- scripts/prep_2.x_autoinstall.py
+++ scripts/prep_2.x_autoinstall.py
@@ -24,35 +24,24 @@ parser.add_argument("-n", "--vm_node",
                     help='vm node name where esgf node is to be installed')
 parser.add_argument("-H", "--vm_jenkins_home", 
                     help='vm node name jenkins home')
-parser.add_argument("-t", "--test_suite_node", 
-                    help='name of the node where esgf-test-suite is to be run from')
-parser.add_argument("-w", "--test_suite_node_jenkins_home", 
-                    help='user jenkins home on the node where esgf-test-suite is to be run from')
 
 args = parser.parse_args()
 conf_dir = args.conf_dir
 vm_node = args.vm_node
 vm_jenkins_home = args.vm_jenkins_home
-test_suite_node = args.test_suite_node
-test_suite_node_jenkins_home = args.test_suite_node_jenkins_home
 autoinstall_conf = 'esg-autoinstall.conf'
 cp_config_cmd = "scp {c}/2.x/{conf}.{n} {n}:/tmp/{conf}".format(c=conf_dir,
                                                                 conf=autoinstall_conf,
                                                                 n=vm_node)
 source = "{c}/esgf-test-suite/{n}_config.ini".format(c=conf_dir,
                                                      n=vm_node)
-dest = "{ts}:{ts_jh}/esgf".format(n=vm_node,
-                                  ts=test_suite_node,
-                                  ts_jh=test_suite_node_jenkins_home)
 
 vm_dest = "{n}:{vm_jh}/esgf".format(n=vm_node,
                                     vm_jh=vm_jenkins_home)
 
-cp_ts_conf_cmd = "scp {s} {d}".format(s=source, d=dest)
 cp_vm_conf_cmd = "scp {s} {d}".format(s=source, d=vm_dest)
 
 cmds = [cp_config_cmd,
-        cp_ts_conf_cmd,
         cp_vm_conf_cmd
         ]
 for cmd in cmds:
