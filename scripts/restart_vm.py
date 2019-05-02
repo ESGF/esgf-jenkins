de0765cbec73107f564ac16e088a1caa325352f5
Muryanto <muryanto1@ML-9516678.llnl.gov>
2019-05-02 11:57:31 -0700

    add restarting vm after install

---
+++ scripts/restart_vm.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import argparse
+import re
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+modules_dir = os.path.join(this_dir, '..', 'modules')
+sys.path.append(modules_dir)
+
+#from Util import *
+from vm_util import *
+
+parser = argparse.ArgumentParser(description="prepare a virtual machine",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("-H", "--vm_host", help="vm_host", required=True)
+parser.add_argument("-x", "--vmx", help="full path vmx file name", required=True)
+parser.add_argument("-s", "--snapshot", help="vm snapshot to use", required=True)
+parser.add_argument("-n", "--vm_node", help="vm node", required=True)
+args = parser.parse_args()
+
+vm_host = args.vm_host
+vmx = args.vmx
+vm_snapshot = args.snapshot
+vm_node = args.vm_node
+
+# check if vm is running (note that there may be a vm with different vmx file
+# for this vm that is running.
+vmx_filename = check_if_vm_is_running(vm_host, vm_node)
+
+ret_code = stop_vm(vm_host, vmx_filename)
+if ret_code != SUCCESS:
+    sys.exit(ret_code)
+
+ret_code = start_vm(vm_host, vmx)
+
+sys.exit(ret_code)
+
